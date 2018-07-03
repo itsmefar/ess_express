e42722a33c9ad9465ea352364d4e70c591ca0fad
itsmeFarizan <chocolatez_143@yahoo.com>
2018-07-03 16:29:31 +0800

    back-end for employee

--- routes/user.js
+++ routes/user.js
@@ -9,38 +9,49 @@ var { User } = require('../models/user.model');
 
 //create user
 router.post('/', (req, res) => {
-    //var userBody = _.pick(req.body, ['email', 'password', 'name', 'nric', 'contact', 'firstName', 'lastName', 'address']);
-    var user = new User(req.body);
-    //console.log(userBody);
-    //save user
-    user.save().then((data) => {
-      res.status(200).send(data);
-    }).catch((e) => {
-      console.log(e);
-      res.status(400).send(e);
-    })
-  });
-  
-  // user login
-  router.post('/login', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
-    console.log(body);
-    User.findByCredentials(body.email, body.password).then((user) => {
-      return user.generateAuthToken().then((token) => {
-        res.json({token: token, user: user });
-      });
-    }).catch((e) => {
-      res.status(403).send(e);
+  //var userBody = _.pick(req.body, ['email', 'password', 'name', 'nric', 'contact', 'firstName', 'lastName', 'address']);
+  var user = new User(req.body);
+  //console.log(userBody);
+  //save user
+  user.save().then((data) => {
+    res.status(200).send(data);
+  }).catch((e) => {
+    console.log(e);
+    res.status(400).send(e);
+  })
+});
+
+// user login
+router.post('/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  console.log(body);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.json({ token: token, user: user });
     });
+  }).catch((e) => {
+    res.status(403).send(e);
   });
-  
-  //get current user
-  router.get('/getuser', (req, res) => {
-    var id = req.user._id;
-    User.findByUserId(id).then((user) => {
-      res.status(200).send(user);
-    }, (err) => {
-      res.status(400).send(err);
-    });
+});
+
+//get current user
+router.get('/getuser', (req, res) => {
+  var id = req.user._id;
+  User.findByUserId(id).then((user) => {
+    res.status(200).send(user);
+  }, (err) => {
+    res.status(400).send(err);
   });
-  module.exports = router;
+});
+
+//get all user
+router.get('/getalluser', (req, res) => {
+  User.find({}).then(data => {
+    res.status(200).send(data);
+
+  }).catch(error => {
+    res.status(400).send(error);
+  })
+});
+
+module.exports = router;
